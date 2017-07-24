f1a5aa617490b9214b16982442c396c845bbee72
hansam <googzishuai@163.com>
2017-07-24 09:42:23 +0800

    add httptest.java

---
+++ src/main/java/cc/hansam/java/net/http/HttpTest.java
@@ -0,0 +1,32 @@
+package cc.hansam.java.net.http;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.http.protocol.HTTP;
+
+/**
+ * @author hansam 945210972@qq.com
+ * @date 2017年7月21日下午5:39:45
+ */
+public class HttpTest {
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("http://192.168.1.124:1532/simple/shi");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, HTTP.UTF_8));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
