770f865f13f295d7f03689d8cb32f0c7696b7a83
hansam <googzishuai@163.com>
2017-07-24 14:29:39 +0800

    test

--- src/main/java/cc/hansam/java/net/http/HttpTest.java
+++ src/main/java/cc/hansam/java/net/http/HttpTest.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.apache.http.protocol.HTTP;
-
 /**
  * @author hansam 945210972@qq.com
  * @date 2017年7月21日下午5:39:45
@@ -19,7 +17,7 @@ public class HttpTest {
 			URL url = new URL("http://192.168.1.124:1532/simple/shi");
 			URLConnection connection = url.openConnection();
 			InputStream is = connection.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, HTTP.UTF_8));
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"),1024);
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
---
+++ src/main/java/cc/hansam/java/net/rest/RestTest.java
@@ -0,0 +1,59 @@
+package cc.hansam.java.net.rest;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+
+import javax.print.attribute.ResolutionSyntax;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * @author hansam 945210972@qq.com
+ * @date 2017年7月24日上午11:44:16
+ */
+public class RestTest {
+
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet("http://192.168.1.124:1532/simple/shi");
+		HttpResponse response = httpclient.execute(httpget);
+		
+		// 响应成功
+		if (response.getStatusLine().getStatusCode() == 200) {
+			HttpEntity entity = response.getEntity();
+
+			StringBuilder sb = new StringBuilder();
+			String line;
+
+			if (entity != null) {
+				InputStream instream = entity.getContent();
+				BufferedReader br = new BufferedReader(new InputStreamReader(instream, HTTP.UTF_8), 1024);
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+				}
+			}
+			String result = sb.toString();
+			System.out.println(result);
+
+			Map map = JSONObject.parseObject(result,Map.class);
+			
+			System.out.println(map);
+			System.out.println(map.get(("data")));
+		}
+	}
+}
