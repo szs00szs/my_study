9cd94fffefcedbda44bbf182d2386213a035a25f
hansam <googzishuai@163.com>
2017-09-13 09:50:37 +0800

    c

--- src/test/java/cc/hansam/test/App.java
+++ src/test/java/cc/hansam/test/App.java
@@ -1,15 +1,24 @@
 package cc.hansam.test;
 
+import java.lang.reflect.Method;
+
 /**
  * @author hansam 945210972@qq.com
  * @date 2017年7月27日下午3:18:22
  */
 public class App {
 	public static void main(String[] args) {
-
-		String a = "a";
-		String b = "b";
-		a.compareTo(b);
+		String JDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+			try {
+				Class<?> forName = Class.forName(JDriver);
+				Method[] methods = forName.getMethods();
+				for (Method method : methods) {
+					System.out.println(method);
+				}
+				System.out.println(forName.getMethods());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 
 	}
 }
