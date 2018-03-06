e675982fb9297d63691f655f7ce3f0b2b516ac69
shishuai <945210972@qq.com>
2018-03-06 16:54:55 +0800

    add method

---
+++ src/main/java/cc/hansam/java/redis/RedisTest.java
@@ -0,0 +1,18 @@
+package cc.hansam.java.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by ShiSS on 2018/3/6.
+ */
+public class RedisTest {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("192.168.19.108");
+        jedis.auth("1234");
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+
+    }
+}
